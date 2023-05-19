f3640b9e4b21cbde1198b7fbbe67878f373b3ff3
BrunoSong <brunosong@wjtb.net>
2023-05-19 17:57:11 +0900

    깃 이그노어 추가

--- src/main/java/hello/ActuatorApplication.java
+++ src/main/java/hello/ActuatorApplication.java
@@ -1,13 +1,38 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ActuatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+
+    //http://localhost:8080/actuator/httpexchanges 이렇게 확인이 가능하다. 최대 100개까지 메모리 디비에 올라간다.
+    //개발단계에서만 사용하는걸 추천한다.
+    //운영에서는 pinpoint 를 사용하는걸 추천한다고 한다.
+
+    /*
+    * 보안!!!!
+    * 내부적으로 너무 많은 정보를 제공하기때문에 보안을 설정을 해야한다.
+    * 엑츄에이터 엔드포인트들은 외부인터넷에서 막는게 좋다. 내부에서만 접근 가능한 내부망을 사용하는 것이 안전하다.
+    *
+    * 엑츄에이터 포트를 설정할수 있다.
+    * management.server.port=9292 이런식으로 들어갈수 있다.
+    * 
+    * 경로에 대한 보안을 처리를 해야 한다. 스프링 시큐리티를 사용하든 필터를 사용하든 인터셉트를 사용하든간에
+    *
+    * 엔드포인트 경로도 변경이 가능하다 /actuator /manage 이런식으로
+    * */
+
+    @Bean
+    public InMemoryHttpExchangeRepository inMemoryHttpExchangeRepository(){
+        return new InMemoryHttpExchangeRepository();
     }
 
 }
---
+++ src/main/java/hello/contoller/LogController.java
@@ -0,0 +1,32 @@
+package hello.contoller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogController {
+
+    @GetMapping("/log")
+    public String log(){
+
+        //http://localhost:8080/actuator/loggers
+        //여기를 들어가보면 전체적으로 어떤 로그레벨을 확인할수 있다.
+        //실시간으로 로그레벨 변경도 가능하다.
+        //실시간으로 info에서 debug로 변경을 해서 운영서버에서 확인 할수 있는 기능이다.
+        //http://localhost:8080/actuator/loggers/hello.controller 이렇게 post로 바디에 json으로 { "configuredLevel" : "TRACE" } 로 날리면 된다.
+
+        log.trace("trace LogController");
+        log.debug("debug LogController");
+        log.info("info LogController");
+        log.warn("warn LogController");
+        log.error("error LogController");
+
+        return "ok";
+
+    }
+
+
+}
